26641de309eec5df74bee21f56b49b5ab6b346bf
Sebas Sebas <seplarui@gmail.com>
2017-02-11 09:58:50 -0800

    Rutas añadidas

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,46 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+  |--------------------------------------------------------------------------
+  | Web Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register web routes for your application. These
+  | routes are loaded by the RouteServiceProvider within a group which
+  | contains the "web" middleware group. Now create something great!
+  |
+ */
 
 Route::get('/', function () {
-    return view('welcome');
+    return "<h1>Pantalla Principal</h1>";
+});
+
+Route::get('/login', function() {
+
+    return "<h1>Pantalla Login</h1>";
+});
+
+Route::get('/logout', function() {
+
+    return "<h1>Pantalla Logout</h1>";
+});
+
+Route::get('/catalog', function() {
+
+    return "<h1>Pantalla Listado Películas</h1>";
+});
+
+Route::get('/catalog/show/{id}', function() {
+
+    return "<h1>Pantalla Vista detalle de película</h1>";
+});
+
+Route::get('/catalog/create', function() {
+
+    return "<h1>Pantalla Añadir Película</h1>";
+});
+
+Route::get('/catalog/edit/{id}', function() {
+
+    return "<h1>Pantalla Modificar película</h1>";
 });
